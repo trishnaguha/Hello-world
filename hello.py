c937e48fd8dc35637a9a5e9aab00faf713898055
Tridev <tridevguha17@gmail.com>
2015-08-08 09:20:20 +0530

    Update to Hello-world

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python3
 print("Hello World")
 print("I am learning git and github.It's cool!")
+print("I am surprised to know that github gives unlimited access to create new repositories and unlimites access to projects!")
+print("It is really cool how they manages it!")
+print("Visit this channel in irc to ask questions related to github:  #github")
